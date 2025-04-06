a95c5d56c2cdc18ebc8088e04f56ec539d0e1edd
FreshLikeABreeze <mrmilk917@yahoo.com>
2025-04-06 15:45:19 -0400

    added static firebase code

---
+++ static/config.js
@@ -0,0 +1,19 @@
+/**
+ * Update this file with your Firebase configuration settings.
+ * API key and auth domain are required.
+ */
+
+/**
+ * ++++ ADD YOUR FIREBASE CONFIGURATION BELOW ++++
+ * === Firebase Apikey and Domain ===
+ */
+
+
+/**
+ * Firebase configuration
+ * @type {{apiKey: string, authDomain: string}}
+ */
+const config = {
+    apiKey: "AIzaSyCMbZo11UqXz-SUu42Rb_Qrw1TLQHS4mQY",
+    authDomain: "cloud-run-454615.firebaseapp.com",
+};
